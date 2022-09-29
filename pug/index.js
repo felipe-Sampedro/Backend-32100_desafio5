2d62b32f805ad64deb27cd647a131f66888debb8
felipe.sampedro <pipesancho182@gmail.com>
2022-09-28 23:22:39 -0500

    motor de plantilla pug por fin terminado, es basicamente lo mismo de handlebars pero con su estructura unica

--- pug/index.js
+++ pug/index.js
@@ -1,34 +1,26 @@
-const express = require('express')
 const path = require('path')
-const {engine:handlebars}=require('express-handlebars')
-const Products = require('./model/products')
-
 const productsRoutes = require('./routes')
-
-const PORT = process.env.PORT || 8080
+const Products = require('./model/products')
+const express = require('express')
 
 const app = express()
+const PORT = process.env.PORT || 8080
+
 const products = new Products()
 
-app.engine('hbs',handlebars({
-    extname:'hbs',
-    defaultLayout:'index.hbs',
-    layoutsDir: path.resolve(__dirname,'./views/layouts'),
-    partialsDir:path.resolve(__dirname,'./views/partials')
-}))
-
-app.set('views','./views/layouts');
-app.set('view engine','hbs')
+app.set('views','./views');
+app.set('view engine','pug')
 
 // app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.render('main',{showp:true,products:products.getAll()})
-})
 
 app.use('/productos',productsRoutes)
+
+app.get('/',(req,res)=>{
+    res.render('index')
+})
 
 app.use((err, req, res, next) => {
 	console.log(err);
